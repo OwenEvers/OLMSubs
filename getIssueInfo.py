f38186bd65718b9f9942d369abe25e52efdbadf3
ope <patrickevers1963@gmail.com>
2023-03-15 19:42:32 +0000

    Version 1. added try: for file not found

--- getIssueInfo.py
+++ getIssueInfo.py
@@ -5,26 +5,52 @@ import PyPDF2
 
 
 def get_Metadata(pdf_file):                     # pass in the pdf file
-    with open(pdf_file, 'rb') as pdf_file:
-        # Create a PDF reader object
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        info = pdf_reader.metadata              # info contains ALL details: can return
-        print(info)                             # metadata in dictionary
-        author = info.author                    # author name
-        print(author)
-        title = info.title                      # pdf title
-        print(title)
-        pages = len(pdf_reader.pages)           # number of pages
-        print(pages)                            # can return any or all above
 
+    direct = "Mags/"
+    num = str(pdf_file)
+    ext = ".pdf"
+    pdf = f"{direct}{num}{ext}"
+    try:
+        with open(pdf, 'rb') as pdf_file:
+            # Create a PDF reader object
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            info = pdf_reader.metadata  # info contains ALL details: can return metadata in dictionary
+            #print(info)
+            author = info.author  # author name
+            title = info.title  # pdf title
+            pages = len(pdf_reader.pages)  # number of pages
+
+    except FileNotFoundError:
+        pdf = 'error.pdf'
+        with open(pdf, 'rb') as pdf_file:
+            # Create a PDF reader object
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            info = pdf_reader.metadata  # info contains ALL details: can return metadata in dictionary
+            #print(info)
+            author = info.author  # author name
+            title = info.title  # pdf title
+            pages = len(pdf_reader.pages)  # number of pages
+
+    #print(info)
+    return info
 
 def extract_pg1_text(pdf_file):                     # pass in the pdf file
-    with open(pdf_file, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-        textpg1 = pdf_reader.pages[0].extract_text()  # text on page 1 to return
-        print(textpg1)
+    direct = "Mags/"
+    num = str(pdf_file)
+    ext = ".pdf"
+    pdf = f"{direct}{num}{ext}"
+    try:
+        with open(pdf, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            textpg1 = pdf_reader.pages[0].extract_text()  # text on page 1 to return
+            #print(textpg1)
+    except FileNotFoundError:
+        pdf = 'error.pdf'
+        with open(pdf, 'rb') as pdf_file:
+            pdf_reader = PyPDF2.PdfReader(pdf_file)
+            textpg1 = pdf_reader.pages[0].extract_text()  # text on page 1 has error to return
+    return textpg1
 
-
-pdf_file = 'Mags/42.pdf'                                # test pdf
-get_Metadata(pdf_file)                                  # call
+#pdf_file = '53'                                # test Number only .pdf
+#get_Metadata(pdf_file)                                  # call
 #extract_pg1_text(pdf_file)                              # call
